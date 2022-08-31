90e066adc036644d33dc915aa5b259ebcb059714
NinjaAung <ninja.aung02@gmail.com>
2022-08-30 23:32:41 -0700

    Added Support for finding pages with minimal content/owned by domain

--- go.mod
+++ go.mod
@@ -2,6 +2,36 @@ module github.com/NinjaAung/supersite_scrapper
 
 go 1.18
 
-require github.com/gocarina/gocsv v0.0.0-20220823132111-71f3a5cb2654
+require (
+	github.com/gocarina/gocsv v0.0.0-20220823132111-71f3a5cb2654
+	github.com/joho/godotenv v1.4.0
+	golang.org/x/oauth2 v0.0.0-20220822191816-0ebed06d0094
+	google.golang.org/api v0.91.0
+)
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require (
+	cloud.google.com/go/compute v1.9.0 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/htmlquery v1.2.5 // indirect
+	github.com/antchfx/xmlquery v1.3.12 // indirect
+	github.com/antchfx/xpath v1.2.1 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220804142021-4e6b2dfa6612 // indirect
+	google.golang.org/grpc v1.48.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -12,10 +13,68 @@ import (
 	"strings"
 
 	"github.com/gocarina/gocsv"
+	"github.com/gocolly/colly"
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/sheets/v4"
 )
 
 // Name, Address, Website, Phone Reviews, Rating, Category, Verified
+
+func errCheck(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	godotenv.Load()
+
+	fmt.Println("Reading and Combing All CSV")
+	businesses := ReadAll() // Reads all csv in current directory
+
+	var reviews []Businesses
+	var flaggeds []Businesses
+	var emptys []Businesses
+
+	fmt.Println("Reading Data")
+	for _, business := range businesses {
+		business.Website = strings.TrimSpace(business.Website)
+		business.CID = strings.TrimSpace(business.CID)
+		switch business.Website {
+		case "":
+			emptys = append(emptys, business)
+		case "http://business.site", "http://godaddysites.com":
+			business.Website = findWebsite(business.CID, os.Getenv("API_KEY"))
+			// fmt.Printf("%s | <Found Missing Website>\n", business.Website)
+			fallthrough
+		default:
+			curl(business, &flaggeds, &reviews)
+		}
+	}
+	// Push Finalization
+	pushToSheet(flaggeds, emptys, reviews)
+}
+
+func reviewPages(url string) bool {
+	c := colly.NewCollector()
+
+	counter := 0
+	counter_error := 0
+
+	c.OnHTML("body", func(e *colly.HTMLElement) { counter += len([]rune(e.Text)) })
+
+	c.OnError(func(r *colly.Response, err error) { counter_error += 1 })
+
+	c.Visit(url)
+
+	if counter <= 2000 || counter_error > 0 {
+		return true
+	}
+	return false
+}
+
+// CSV READER
 type Businesses struct {
 	Name     string `csv:"Name"`
 	Address  string `csv:"Address"`
@@ -26,12 +85,6 @@ type Businesses struct {
 	Verified string `csv:"Merchant Verified"`
 	Category string `csv:"Category"`
 	CID      string `csv:"Listing CID"`
-}
-
-func errCheck(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
 }
 
 func csvReader(file string, business *[]Businesses) {
@@ -53,21 +106,32 @@ func ReadAll() []Businesses {
 	return business
 }
 
-func curl(url string) {
+// Checkers
+func curl(business Businesses, flagged *[]Businesses, review *[]Businesses) {
+	url := business.Website
 	cmd := exec.Command("curl", "-I", "-m", "5", url)
 	stdout, err := cmd.Output()
 	if err != nil {
-		fmt.Printf("%s | <FLAG>\n", url)
+		// fmt.Printf("%s | <FLAG>\n", url)
+		*flagged = append(*flagged, business)
+
 		return
 	}
 	data := strings.Split(string(stdout), "\n")
 	statusCode := strings.TrimSpace(string([]rune(strings.Split(data[0], " ")[1])[0]))
 	switch statusCode {
 	case "3", "2":
+		if reviewPages(url) {
+			*flagged = append(*flagged, business)
+			return
+		}
+		*review = append(*review, business)
 	case "4":
-		fmt.Printf("%s | <NOT FOUND>\n", url)
+		// fmt.Printf("%s | <NOT FOUND>\n", url)
+		*flagged = append(*flagged, business)
 	default:
-		fmt.Printf("%s | %s\n", url, statusCode)
+		// fmt.Printf("%s | %s\n", url, statusCode)
+		*flagged = append(*flagged, business)
 	}
 }
 
@@ -101,22 +165,51 @@ func findWebsite(cid string, apiKey string) string {
 	return places.Result.Website
 }
 
-func main() {
-	var businesses []Businesses
+func pushToSheet(flagged []Businesses, empty []Businesses, review []Businesses) {
+	ctx := context.Background()
 
-	godotenv.Load()
+	b, err := ioutil.ReadFile("supersite-scraping.json")
+	errCheck(err)
+	conf, err := google.JWTConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
+	errCheck(err)
 
-	csvReader("bad.csv", &businesses)
-	for _, business := range businesses {
-		website := strings.TrimSpace(business.Website)
-		cid := strings.TrimSpace(business.CID)
-		switch website {
-		case "http://business.site", "http://godaddysites.com":
-			website = findWebsite(cid, os.Getenv("API_KEY"))
-			fmt.Printf("%s | <Found Mssing Website>\n", website)
-			fallthrough
-		default:
-			curl(website)
-		}
+	client := conf.Client(ctx)
+
+	srv, err := sheets.New(client)
+	errCheck(err)
+
+	spreadsheetID := os.Getenv("SPREADSHEET_ID")
+
+	BatchUpdate("Flagged", flagged, spreadsheetID, srv, ctx)
+	BatchUpdate("Empty", empty, spreadsheetID, srv, ctx)
+	BatchUpdate("Review", review, spreadsheetID, srv, ctx)
+
+}
+
+func BatchUpdate(name string, data []Businesses, id string, srv *sheets.Service, ctx context.Context) {
+
+	fmt.Printf("Pushing %s data to Sheets", name)
+	rb := sheets.BatchUpdateValuesRequest{
+		ValueInputOption: "USER_ENTERED",
 	}
+
+	rb.Data = append(rb.Data, &sheets.ValueRange{
+		Range:  fmt.Sprintf("'%s'!A2:I500", name),
+		Values: csvToInterface(data),
+	})
+
+	_, err := srv.Spreadsheets.Values.BatchUpdate(id, &rb).Context(ctx).Do()
+	errCheck(err)
+	fmt.Println("Done")
+
+}
+
+func csvToInterface(businesses []Businesses) [][]interface{} {
+	var data [][]interface{}
+
+	for _, row := range businesses {
+		row_data := []interface{}{row.Name, row.Address, row.Website, row.Phone, row.Reviews, row.Rating, row.Verified, row.Category}
+		data = append(data, row_data)
+	}
+	return data
 }
