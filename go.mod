af5804e465a74cf41e1f45d01e1d90d6a5ff60d1
NinjaAung <ninja.aung02@gmail.com>
2022-09-07 18:46:21 -0700

    Added Snap Scrapper and refactored code

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.18
 
 require (
 	github.com/gocarina/gocsv v0.0.0-20220823132111-71f3a5cb2654
+	github.com/gocolly/colly v1.2.0
 	github.com/joho/godotenv v1.4.0
 	golang.org/x/oauth2 v0.0.0-20220822191816-0ebed06d0094
 	google.golang.org/api v0.91.0
+	googlemaps.github.io/maps v1.3.2
 )
 
 require (
@@ -17,7 +19,6 @@ require (
 	github.com/antchfx/xmlquery v1.3.12 // indirect
 	github.com/antchfx/xpath v1.2.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
@@ -30,6 +31,7 @@ require (
 	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
 	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220804142021-4e6b2dfa6612 // indirect
 	google.golang.org/grpc v1.48.0 // indirect
--- main.go
+++ main.go
@@ -3,74 +3,133 @@ package main
 import (
 	"context"
 	"encoding/csv"
-	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"path"
 	"path/filepath"
+	"regexp"
 	"strings"
+	"unicode"
 
 	"github.com/gocarina/gocsv"
 	"github.com/gocolly/colly"
 	"github.com/joho/godotenv"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/sheets/v4"
+	"googlemaps.github.io/maps"
 )
 
-// Name, Address, Website, Phone Reviews, Rating, Category, Verified
-
-func errCheck(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+type Business struct {
+	Business string `csv:"Business"` // same as name
+	Name     string `csv:"Name"`
+	Email    string `csv:"Email"`
+	Address  string `csv:"Address"`
+	City     string `csv:"City"`
+	State    string `csv:"State"`
+	Zip      string `csv:"Zip"`
+	Website  string `csv:"Website"`
+	Phone    string `csv:"Phone"`
+	Reviews  string `csv:"Reviews"`
+	Rating   string `csv:"Ratings"`
+	Verified string `csv:"Merchant Verified"`
+	Category string `csv:"Category"`
+	CID      string `csv:"Listing CID"`
+	PlaceID  string `csv:"PlaceID"`
 }
 
 func main() {
+	// Init Vars
+	var (
+		reviews  []Business
+		flaggeds []Business
+		emptys   []Business
+	)
 
+	// Move to working directory to exec
 	e, _ := os.Executable()
-	fmt.Printf("Reading and combing all CSV in: %s\n\n\n", path.Dir(e))
-	godotenv.Load(path.Dir(e) + "/" + ".env")
-	businesses := ReadAll(path.Dir(e))
-	var reviews []Businesses
-	var flaggeds []Businesses
-	var emptys []Businesses
+	os.Chdir(path.Dir(e))
+
+	// Program Start
+	godotenv.Load()
+	businesses := readAll(os.Getenv("FILE_PATH"))
+
+	// Google API Interface
+	ctx := context.Background()
+	mClient, err := maps.NewClient(maps.WithAPIKey(os.Getenv("API_KEY")))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Printf("\n\nParsing %v Business Records\n", len(businesses))
+	if os.Getenv("SNAP") == "true" {
+		var bCells []Business
+		for _, b := range businesses {
+			b.Name = b.Business
+			b.PlaceID = findPlaceID(ctx, mClient, numberParse(b.Phone))
+			if b.PlaceID != "" {
+				b.Website = findWebsite(ctx, mClient, b.PlaceID)
+			}
+			bCells = append(bCells, b)
+		}
+		if len(bCells) != 0 {
+			businesses = bCells
+		}
+	}
 	for _, business := range businesses {
-		business.Website = strings.TrimSpace(business.Website)
-		business.CID = strings.TrimSpace(business.CID)
+		if strings.ToLower(business.Reviews) == "no" {
+			business.Reviews = "0"
+		}
+
 		switch business.Website {
 		case "":
 			emptys = append(emptys, business)
-		case "http://business.site", "http://godaddysites.com":
-			fmt.Println(path.Dir(e) + "/" + os.Getenv("API_KEY"))
-			business.Website = findWebsite(business.CID, path.Dir(e)+"/"+os.Getenv("API_KEY"))
-			// fmt.Printf("%s | <Found Missing Website>\n", business.Website)
+		case "http://godaddysites.com", "http://business.site":
+			queryNumber := findPlaceID(ctx, mClient, numberParse(business.Website))
+
+			if queryNumber == "" {
+				emptys = append(emptys, business)
+				break
+			}
+
+			business.Website = findWebsite(ctx, mClient, queryNumber)
+			if business.Website == "" {
+				emptys = append(emptys, business)
+				break
+			}
+
 			fallthrough
 		default:
 			curl(business, &flaggeds, &reviews)
 		}
 	}
+	// Clean Duplicates
+
 	// Push Finalization
-	fmt.Println("\n\n\nBatch Push")
+	fmt.Println("\nBatch Push")
 	pushToSheet(flaggeds, emptys, reviews, path.Dir(e))
 }
 
-func reviewPages(url string) bool {
+func numberParse(number string) string {
+	p := "+1"
+	for _, char := range number {
+		if unicode.IsDigit(char) {
+			p += string(char)
+		}
+	}
+	return p
+}
+
+func isPageValid(url string) bool {
 	c := colly.NewCollector()
 
 	counter := 0
 	counter_error := 0
 
 	c.OnHTML("body", func(e *colly.HTMLElement) { counter += len([]rune(e.Text)) })
-
 	c.OnError(func(r *colly.Response, err error) { counter_error += 1 })
-
 	c.Visit(url)
 
 	if counter <= 2000 || counter_error > 0 {
@@ -79,50 +138,23 @@ func reviewPages(url string) bool {
 	return false
 }
 
-// CSV READER
-type Businesses struct {
-	Name     string `csv:"Name"`
-	Address  string `csv:"Address"`
-	Website  string `csv:"Website"`
-	Phone    string `csv:"Phone"`
-	Reviews  string `csv:"Reviews"`
-	Rating   string `csv:"Ratings"`
-	Verified string `csv:"Merchant Verified"`
-	Category string `csv:"Category"`
-	CID      string `csv:"Listing CID"`
-}
+func isPageHasFlash(url string) bool {
+	c := colly.NewCollector()
+	text := ""
+	c.OnHTML("script", func(e *colly.HTMLElement) { text += e.Text })
+	c.Visit(url)
 
-func csvReader(file string, business *[]Businesses) {
-	gocsv.SetCSVReader(func(in io.Reader) gocsv.CSVReader { r := csv.NewReader(in); r.LazyQuotes = true; return r })
-	bytes, err := ioutil.ReadFile(file)
-	errCheck(err)
-
-	err = gocsv.UnmarshalBytes(bytes, business)
-	errCheck(err)
-}
-
-func ReadAll(filePath string) []Businesses {
-	files, err := filepath.Glob(filePath + "/*.csv")
-
-	errCheck(err)
-	var business []Businesses
-	for _, file := range files {
-		var temp []Businesses
-		fmt.Println("Loading: ", file)
-		csvReader(file, &temp)
-		business = append(business, temp...)
-	}
-
-	return business
+	fmt.Println(regexp.MatchString(".swf", text))
+	return false
 }
 
 // Checkers
-func curl(business Businesses, flagged *[]Businesses, review *[]Businesses) {
-	url := business.Website
+func curl(business Business, flagged *[]Business, review *[]Business) {
+	url := strings.TrimSpace(business.Website)
 	cmd := exec.Command("curl", "-I", "-m", "5", url)
 	stdout, err := cmd.Output()
 	if err != nil {
-		// fmt.Printf("%s | <FLAG>\n", url)
+		fmt.Printf("%s | <FLAG>\n", url)
 		*flagged = append(*flagged, business)
 
 		return
@@ -131,94 +163,128 @@ func curl(business Businesses, flagged *[]Businesses, review *[]Businesses) {
 	statusCode := strings.TrimSpace(string([]rune(strings.Split(data[0], " ")[1])[0]))
 	switch statusCode {
 	case "3", "2":
-		if reviewPages(url) {
+		if isPageValid(url) {
 			*flagged = append(*flagged, business)
 			return
 		}
+
+		if isPageHasFlash(url) {
+			*flagged = append(*flagged, business)
+			return
+		}
+
 		*review = append(*review, business)
 	case "4":
-		// fmt.Printf("%s | <NOT FOUND>\n", url)
 		*flagged = append(*flagged, business)
 	default:
-		// fmt.Printf("%s | %s\n", url, statusCode)
 		*flagged = append(*flagged, business)
 	}
 }
 
-func findWebsite(cid string, apiKey string) string {
-	type PlacesResult struct {
-		Website string `json:"website"`
+// CSV
+
+func csvReader(filePath string, business *[]Business) {
+	gocsv.SetCSVReader(func(in io.Reader) gocsv.CSVReader {
+		r := csv.NewReader(in)
+		r.LazyQuotes = true
+		r.TrimLeadingSpace = true
+		return r
+	})
+	bytes, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal("Could not read file", filePath, err)
 	}
-	type Places struct {
-		HmtlAttribution []string     `json:"html_attribution"`
-		Result          PlacesResult `json:"result"`
-		Status          string       `json:"status"`
+	err = gocsv.UnmarshalBytes(bytes, business)
+	if err != nil {
+		log.Fatal("Could not parse csv", filePath, err)
 	}
-
-	url := fmt.Sprintf("https://maps.googleapis.com/maps/api/place/details/json?cid=%s&key=%s", cid, apiKey)
-	method := "GET"
-
-	client := &http.Client{}
-	req, err := http.NewRequest(method, url, nil)
-	errCheck(err)
-
-	res, err := client.Do(req)
-	errCheck(err)
-	defer res.Body.Close()
-
-	var places Places
-
-	body, err := ioutil.ReadAll(res.Body)
-	errCheck(err)
-	json.Unmarshal(body, &places)
-
-	return places.Result.Website
 }
 
-func pushToSheet(flagged []Businesses, empty []Businesses, review []Businesses, filePath string) {
-	ctx := context.Background()
-	fmt.Println(filePath + "/" + os.Getenv("JWT_TOKEN"))
-	b, err := ioutil.ReadFile(filePath + "/" + os.Getenv("JWT_TOKEN"))
-	errCheck(err)
-	conf, err := google.JWTConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
-	errCheck(err)
+func readAll(filePath string) []Business {
+	files, err := filepath.Glob(filePath + "*.csv")
+	if err != nil {
+		log.Fatal("Could not read files", err)
+	}
+	var business []Business
+	for _, file := range files {
+		var temp []Business
+		fmt.Println("Loading: ", file)
+		csvReader(file, &temp)
+		business = append(business, temp...)
+	}
+	return business
+}
 
-	client := conf.Client(ctx)
+// Maps
 
-	srv, err := sheets.New(client)
-	errCheck(err)
+func findWebsite(ctx context.Context, mClient *maps.Client, PlaceID string) string {
+	req := &maps.PlaceDetailsRequest{
+		PlaceID: PlaceID,
+		Fields:  []maps.PlaceDetailsFieldMask{"website"},
+	}
+	resp, err := mClient.PlaceDetails(ctx, req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return resp.Website
+
+}
+
+func findPlaceID(ctx context.Context, mClient *maps.Client, query string) string {
+	req := &maps.FindPlaceFromTextRequest{
+		Input:     query,
+		InputType: "phonenumber",
+		Fields:    []maps.PlaceSearchFieldMask{"place_id"},
+	}
+	resp, err := mClient.FindPlaceFromText(ctx, req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(resp.Candidates) != 0 {
+		return resp.Candidates[0].PlaceID
+	}
+	return ""
+}
+
+// Sheets
+
+func csvToInterface(businesses []Business) [][]interface{} {
+	var data [][]interface{}
+
+	for _, row := range businesses {
+		row_data := []interface{}{row.Name, strings.TrimSpace(fmt.Sprintf("%s %s, %s %s", row.Address, row.City, row.State, row.Zip)), row.Website, row.Phone, row.Reviews, row.Rating, row.Verified, row.Category}
+		data = append(data, row_data)
+	}
+	return data
+}
+
+func BatchUpdate(name string, data []Business, id string, srv *sheets.Service, ctx context.Context) {
+	fmt.Printf("Pushing %s data to Sheets", name)
+	defer fmt.Println("... Done!")
+	rb := sheets.BatchUpdateValuesRequest{ValueInputOption: "USER_ENTERED"}
+	rb.Data = append(rb.Data, &sheets.ValueRange{Range: fmt.Sprintf("'%s'!A2:5000", name), Values: csvToInterface(data)})
+	_, err := srv.Spreadsheets.Values.BatchUpdate(id, &rb).Context(ctx).Do()
+	if err != nil {
+		log.Fatal("Couldn't Update Spreadsheet", err)
+	}
+}
+
+func pushToSheet(flagged []Business, empty []Business, review []Business, filePath string) {
 
 	spreadsheetID := os.Getenv("SPREADSHEET_ID")
+	b, _ := os.ReadFile(os.Getenv("JWT_TOKEN"))
+
+	conf, err := google.JWTConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		log.Fatal("Could not load JWT Service Account JSON file", err)
+	}
+
+	ctx := context.Background()
+	srv, _ := sheets.New(conf.Client(ctx))
 
 	BatchUpdate("Flagged", flagged, spreadsheetID, srv, ctx)
 	BatchUpdate("Empty", empty, spreadsheetID, srv, ctx)
 	BatchUpdate("Review", review, spreadsheetID, srv, ctx)
 
-}
-
-func BatchUpdate(name string, data []Businesses, id string, srv *sheets.Service, ctx context.Context) {
-
-	fmt.Printf("Pushing %s data to Sheets", name)
-	rb := sheets.BatchUpdateValuesRequest{
-		ValueInputOption: "USER_ENTERED",
-	}
-	rb.Data = append(rb.Data, &sheets.ValueRange{
-		Range:  fmt.Sprintf("'%s'!A2:5000", name),
-		Values: csvToInterface(data),
-	})
-
-	_, err := srv.Spreadsheets.Values.BatchUpdate(id, &rb).Context(ctx).Do()
-	errCheck(err)
-	fmt.Println("... Done!")
-
-}
-
-func csvToInterface(businesses []Businesses) [][]interface{} {
-	var data [][]interface{}
-
-	for _, row := range businesses {
-		row_data := []interface{}{row.Name, row.Address, row.Website, row.Phone, row.Reviews, row.Rating, row.Verified, row.Category}
-		data = append(data, row_data)
-	}
-	return data
 }
